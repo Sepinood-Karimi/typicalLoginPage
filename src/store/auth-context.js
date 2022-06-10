d4632fa83cace3b459db16f940b7378e41a1cb70
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-06-10 14:37:41 +0430

    move the whole logic of login and logout to provider

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn:false
+    isLoggedIn:false,
+    onLogout:()=>{},
+    onLogin: (email,password)=>{}
 });
+
+export const AuthContextProvider = ({children})=>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()=>{
+        const userLoggedInInfo=localStorage.getItem('isLoggedIn');
+        if (userLoggedInInfo==='1'){
+            setIsLoggedIn(true);
+        }
+    },[])
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn','1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+    return <AuthContext.Provider value={{isLoggedIn: isLoggedIn,onLogout:logoutHandler,onLogin:loginHandler}}>{children}</AuthContext.Provider>;
+
+}
 
 export default AuthContext;
